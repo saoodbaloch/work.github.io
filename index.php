226360ca38a9edabd84f578ab64a6dad6d7e1205
saoodbaloch <118266529+saoodbaloch@users.noreply.github.com>
2023-05-05 02:38:21 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
+<?php
+$insert = false;
+if(isset($_POST['name'])){
+
+$server = "localhost";
+$name = "root";
+$password = "";
+
+
+$mysql = mysqli_connect($server,$name,$password);
+if (!$mysql){
+    die("you are wrong".mysqli_connect_error());
+}
+
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$gender = $_POST['gender'];
+$age = $_POST['age'];
+$phone = $_POST['phone'];
+$desc = $_POST['desc'];
+
+
+$forms = "INSERT INTO `school`.`forms`(`name`, `email`, `gender`, `age`, `phone`, `DES`) VALUES ('$name','$email','$gender','$age','$phone','$desc')";
+// echo $forms;
+
+if($mysql->query($forms) == true){
+ $insert = true;
+}
+else{
+    echo  $mysql->error;
+}
+$mysql->close();
+};
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,8 +44,13 @@
 </head>
 <body>
     <div class="container">
-        <h1>REGISTRATION FORM</h1>
+    <h1>REGISTRATION FORM</h1>
         <h2>Enter your details submit the form to participation in the picnic</h2>
+       <?php
+       if($insert == true){
+         echo "<p class='paragraph'>thanks for participation</p>";
+       }
+       ?>
         <form action="index.php" method="post">
             <input type="text" name="name" id="name" placeholder="write down in our name">
             <input type="email" name="email" id="email" placeholder="write down in our email">
@@ -21,3 +61,9 @@
         <button class="btn">submit</button>
         </form>
     </div>
+</body>
+</html>
+
+
+
+
